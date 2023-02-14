fd462cd34704a1280a4457adc3f370aff1c7ee62
SNIELSEL <niels@haverkotte.nl>
2023-02-14 13:08:33 +0100

    AddedMusic
    added a button click and main menu theme

--- schooled/Assets/Scripts/UiNav.cs
+++ schooled/Assets/Scripts/UiNav.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class UiNav : MonoBehaviour
 {
@@ -9,29 +10,54 @@ public class UiNav : MonoBehaviour
     public GameObject creditsMenu;
     public GameObject settingsMenu;
 
+    public AudioSource buttonClick;
+
+    public float buttonDelay;
+
     public void ToCredits()
     {
+        buttonClick.Play();
+        StartCoroutine (CreditsCO());
+    }
+
+    public void ToMenu()
+    {
+        buttonClick.Play();
+        StartCoroutine(MenuCO());
+    }
+
+    public void ToSettings()
+    {
+        buttonClick.Play();
+        StartCoroutine(SettingsCO());
+    }
+
+    public void QuitGame()
+    {
+        buttonClick.Play();
+        Application.Quit();
+    }
+
+    public IEnumerator CreditsCO()
+    {
+        yield return new WaitForSeconds(buttonDelay);
         mainMenu.SetActive(false);
         creditsMenu.SetActive(true);
         settingsMenu.SetActive(false);
     }
 
-    public void ToMenu()
+    public IEnumerator MenuCO()
     {
+        yield return new WaitForSeconds(buttonDelay);
         mainMenu.SetActive(true);
         creditsMenu.SetActive(false);
         settingsMenu.SetActive(false);
     }
-
-    public void ToSettings()
+    public IEnumerator SettingsCO()
     {
+        yield return new WaitForSeconds(buttonDelay);
         settingsMenu.SetActive(true);
         creditsMenu.SetActive(false);
         mainMenu.SetActive(false);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 }

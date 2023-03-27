0431c6105b7a34e8d7b1b3acd00935cfdd23d359
SNIELSEL <niels@haverkotte.nl>
2023-03-27 15:24:48 +0200

    Added XML saving to save/load script(needs testing)

--- schooled/Assets/Scripts/SaveAndLoad.cs
+++ schooled/Assets/Scripts/SaveAndLoad.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine.LowLevel;
 
 public class SaveAndLoad : MonoBehaviour
 {
@@ -21,6 +24,9 @@ public class SaveAndLoad : MonoBehaviour
 
     public Button GameLoadbutton;
     public TextMeshProUGUI LoadText;
+
+    private XML_SaveData _SaveData = new XML_SaveData();
+
 
     public void Start()
     {
@@ -73,15 +79,18 @@ public class SaveAndLoad : MonoBehaviour
         PlayerPrefs.SetFloat("PlayerPosY" + slot, playerposition.playerPosy);
         PlayerPrefs.SetFloat("PlayerPosZ" + slot, playerposition.playerPosz);
         PlayerPrefs.SetInt("SaveSlot", slot);
-        
+
+        SaveData();
+        CreateNewSave();
         loadLock.SetMovements();
     }
 
     public void LoadPlayerPos()
     {
-        SetPlayerLocation();
-
         playerposition.Player.position = playerLoc;
+        
+        SetPlayerLocation();
+        LoadData();
     }
 
     public void SetSavePrefs()
@@ -119,5 +128,62 @@ public class SaveAndLoad : MonoBehaviour
     {
         GameLoadbutton = GameObject.Find("LoadButton").GetComponent<Button>();
         LoadText = GameObject.Find("LoadText1").GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SaveData()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(XML_SaveData));
+
+        using (FileStream stream = new FileStream(Application.persistentDataPath + "/SaveData.xml", FileMode.Create))
+        {
+            serializer.Serialize(stream, _SaveData);
+        }
+    }
+
+    public void LoadData()
+    {
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(XML_SaveData));
+
+            using (FileStream stream = new FileStream(Application.persistentDataPath + "/SaveData.xml", FileMode.Open))
+            {
+                _SaveData = serializer.Deserialize(stream) as XML_SaveData;
+            }
+        }
+        catch
+        {
+            SaveData();
+        }
+    }
+
+    public XML_SaveData GetSaveData()
+    {
+        return _SaveData;
+    }
+    public void CreateNewSave()
+    {
+        XML_SaveData newsave = new XML_SaveData();
+        newsave.playerLoc.x = PlayerPrefs.GetFloat("PlayerPosX" + slot);
+        newsave.playerLoc.y = PlayerPrefs.GetFloat("PlayerPosY" + slot);
+        newsave.playerLoc.z = PlayerPrefs.GetFloat("PlayerPosZ" + slot);
+        newsave.slot = PlayerPrefs.GetInt("SaveSlot");
+    }
+
+    [System.Serializable]
+    public class XML_SaveData
+    {
+        public int slot;
+
+        public Vector3 playerLoc;
+        
+        public void Start()
+        {
+            slot = PlayerPrefs.GetInt("SaveSlot");
+
+            playerLoc.x = PlayerPrefs.GetFloat("PlayerPosX" + slot);
+            playerLoc.y = PlayerPrefs.GetFloat("PlayerPosY" + slot);
+            playerLoc.z = PlayerPrefs.GetFloat("PlayerPosZ" + slot);
+        }
     }
 }
